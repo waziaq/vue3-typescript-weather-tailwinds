c40b38dec7a796f5345344bea8053cef2a1fa36f
MuhammadWazexr <mr.zexr2010@gmail.com>
2023-07-19 15:53:39 +0800

    feat: add reusable modal

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,46 @@
+<template>
+    <Transition name="modal-outer">
+        <div v-show="modalActive" @click="$emit('close-modal')"
+            class="absolute w-full bg-black bg-opacity-50 h-screen top-0 left-0 flex justify-center px-8">
+            <Transition name="modal-inner">
+                <div v-if="modalActive"
+                class="p-6 bg-white bg-opacity-90 border self-start mt-32 min-w-full rounded-md max-w-screen-md">
+                <slot />
+                <button @click="$emit('close-modal')" class="text-white bg-weather-primary py-2 px-6 rounded-lg">
+                    Close
+                </button>
+            </div>
+            </Transition>
+        </div>
+    </Transition>
+</template>
+
+<script setup lang="ts">
+defineEmits(["close-modal"]);
+defineProps({
+    modalActive: {
+        type: Boolean,
+        default: false
+    }
+})
+</script>
+
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+    transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+    opacity: 0;
+}
+
+.modal-inner-enter-active {
+    transition: all 0.3s cubic-bezier(0.53, 0.02, 0.19, 1.02) 0.15s;
+}
+
+.modal-inner-leave-active {
+    transition: all 0.3s cubic-bezier(0.53, 0.02, 0.19, 1.02);
+}
+</style>
--- src/components/SiteNavigation.vue
+++ src/components/SiteNavigation.vue
@@ -1,16 +1,31 @@
 <template>
     <header class="sticky top-0">
-        <nav class="container py-4">
+        <nav class="container py-4 relative">
             <p class="gap-2 flex justify-center items-center text-sm text-white">
                 <i class="fa-solid fa-location-arrow text-lg"></i>
                 <span>Melaka, Malaysia</span>
             </p>
+            <p class="absolute top-1/2 right-4 -translate-y-1/2 text-xs text-white space-x-4">
+                <i class="fa-solid fa-pen-to-square"></i>
+                <i @click="toggleModal" class="fa-solid fa-circle-info cursor-pointer"></i>
+            </p>
         </nav>
+        <BaseModal :modal-active="modalActive" @close-modal="toggleModal">
+            <div class="text-black text-sm">
+                <h1>Hy</h1>
+            </div>
+        </BaseModal>
     </header>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import BaseModal from './BaseModal.vue';
 
+const modalActive = ref(false);
+const toggleModal = () => {
+    modalActive.value = !modalActive.value;
+}
 </script>
 
 <style scoped>
