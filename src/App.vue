ebeaeff551adddf5a9cbc10a1ef156e729f4b3e7
MuhammadWazexr <mr.zexr2010@gmail.com>
2023-07-23 01:36:15 +0800

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import SiteNavigation from './components/SiteNavigation.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <div class="flex flex-col min-h-screen font-roboto bg-gradient-to-br from-weather-primary to-weather-secondary">
     <SiteNavigation />
     <RouterView />
+    <Footer />
   </div>
 </template>
 
---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer class="my-8 pt-8">
+        <p class="text-center text-xs text-white font-bold drop-shadow">Made with ❤️ in Malaysia. Copyright &copy; {{ new Date().getFullYear() }} Waziaq</p>
+    </footer>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
